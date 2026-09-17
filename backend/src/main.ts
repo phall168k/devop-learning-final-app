0719180f3fad0b6ef27ae8d7e07420a8b3c802d1
phall168k <phall168k@gmail.com>
2026-09-17 21:46:18 +0700

    chore(backend): configure swagger in main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  setupSwagger(app);
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
