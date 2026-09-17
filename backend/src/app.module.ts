c3d7814f07ca4633d65f78bf36c4b41e24022b68
phall168k <phall168k@gmail.com>
2026-09-17 15:05:46 +0700

    feat(backend): add modules module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ModulesModule,
   ],
   controllers: [],
   providers: [],
