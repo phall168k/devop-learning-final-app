896f2abfe916f25356fc42ba25463ea1134deb5e
phall168k <phall168k@gmail.com>
2026-09-17 15:04:52 +0700

    chore(backend): add config module, enable core, and versioning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(`${process.env.API_PREFIX}`);
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
