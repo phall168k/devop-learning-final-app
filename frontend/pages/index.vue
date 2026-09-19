6893c11250de39639f2d6ad5ee08057a5927d3c2
phall168k <phall168k@gmail.com>
2026-09-19 13:33:09 +0700

    fix(fronted): fix frontend access to api endpoint

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -7,7 +7,7 @@
     const config = useRuntimeConfig();
     const handleGetCategories  = async () => {
         try {
-            const response = await $fetch(`http://api-prod:8000/api/v1/admin/master-data/categories`);
+            const response = await $fetch(`http://nest_backend/api/v1/admin/master-data/categories`);
             console.log('DATA', response);
         } catch (error) {
             console.log(error);
